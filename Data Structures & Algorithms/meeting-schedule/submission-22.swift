d7ca50e9c4e5ca41a6fe306db8e84091865cf8da
Yuhao Chen <yuhao.chen2000@icloud.com>
2026-05-07 19:58:21 -0700

    Add: meeting-schedule - submission-22

---
+++ Data Structures & Algorithms/meeting-schedule/submission-22.swift
@@ -0,0 +1,28 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ *     var start: Int
+ *     var end: Int
+ *     init(_ start: Int, _ end: Int) {
+ *         self.start = start
+ *         self.end = end
+ *     }
+ * }
+ */
+
+class Solution {
+    func canAttendMeetings(_ intervals: [Interval]) -> Bool {
+        if intervals.isEmpty {
+            return true
+        }
+        let sorted = intervals.sorted(by: {$0.start < $1.start})
+
+        for i in 1..<sorted.count {
+            if sorted[i - 1].end > sorted[i].start {
+                return false
+            }
+        }
+        return true
+    }
+}
+
