e70f4aa7f813cc3fd8fcf081e597510f2d625464
Yuhao Chen <yuhao.chen2000@icloud.com>
2026-05-07 20:33:34 -0700

    Add: merge-intervals - submission-26

---
+++ Data Structures & Algorithms/merge-intervals/submission-26.swift
@@ -0,0 +1,20 @@
+class Solution {
+    func merge(_ intervals: [[Int]]) -> [[Int]] {
+        let sorted = intervals.sorted(by: { $0[0] < $1[0] })
+        var result = [sorted[0]]
+
+        for interval in sorted {
+            var start = interval[0]
+            var end = interval[1]
+            var lastEnd = result[result.count - 1][1]
+
+            if start <= lastEnd {
+                result[result.count - 1][1] = max(lastEnd, end)
+            } else {
+                result.append([start, end])
+            }
+        }
+
+        return result
+    }
+}
